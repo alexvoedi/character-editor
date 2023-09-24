5cc98e87b4bde1236ee868cdb2736fb1999853ce
Alexander Vödisch <voedisch.alexander@gmail.com>
2023-09-24 19:57:03 +0200

    ci: add deploy script

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,7 +21,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/character-editor/'),
+  history: createWebHashHistory('/character-editor/'),
   routes,
 })
 
